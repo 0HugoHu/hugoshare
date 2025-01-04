b73762fc159e387c7c117672061153d4001054ea
0HugoHu <0@hugohu.top>
2025-01-04 15:09:27 -0800

    Fix: installation script should in Amazon Linux

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
-apt-get install -y nodejs
-npm install -g yarn pm2
+
+# Update the package manager and install Node.js and Yarn
+sudo dnf update -y
+
+# Install Node.js 22.x
+curl -fsSL https://rpm.nodesource.com/setup_22.x | bash -
+sudo dnf install -y nodejs
+
+# Install Yarn and PM2 globally
+sudo npm install -g yarn pm2
