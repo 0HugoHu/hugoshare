786399f7d39d8ec4516bde3cc5987d895e308634
0HugoHu <0@hugohu.top>
2025-01-04 16:31:01 -0800

    Fix: installation script includes removal of folder

--- scripts/build_application.sh
+++ scripts/build_application.sh
@@ -3,18 +3,6 @@
 # Navigate to the directory
 cd /var/www/sharedrop
 
-# Force stop the service (find and kill all processes started by "nf")
-pkill -f "nf --procfile=Procfile"
-
-# Ensure the service is stopped
-if pgrep -f "nf --procfile=Procfile" > /dev/null; then
-  echo "The service could not be stopped."
-  exit 1
-fi
-
-# Clear the current folder
-rm -rf ./*
-
 # Copy the .env file back into the folder
 cp /home/.env ./.env
 
